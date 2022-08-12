e9cd136f339dfaf805e6c716af2267faa64cad2a
uliasuprun <uliasuprun88@gmail.com>
2022-08-12 16:56:32 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startColorBtn = document.querySelector('[data-start]');
+const stopColorBtn = document.querySelector('[data-stop]');
 
+startColorBtn.addEventListener('click', getStart);
+stopColorBtn.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)}`;
+}
+
+function changeBodyColor() {
+    return body.style.backgroundColor = getRandomHexColor();
+    }
+
+function getStart() {
+    startColorBtn.disabled = true;
+    stopColorBtn.disabled = false;
+    const timerId = setInterval(changeBodyColor, 1000);
+
+    stopColorBtn.addEventListener("click", getStop);
+    function getStop() {
+        startColorBtn.disabled = false;
+        stopColorBtn.disabled = true;
+        clearInterval(timerId);
+    };
+};
